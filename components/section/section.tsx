e63353be925b65a24f40c655812f231d3c86e04e
Yolber JP <yolberjp@gmail.com>
2025-04-05 12:06:41 +0200

    style: Adjust header padding in layout and section components for better alignment and aesthetics

--- components/section/section.tsx
+++ components/section/section.tsx
@@ -18,7 +18,7 @@ export default function Section({
   }>){
     return (
         <section className="flex flex-col pt-20 gap-8 row-start-2 items-center sm:items-start">
-          <header className="px-10 w-full">
+          <header className="px-0 md:px-10 w-full">
             <div className="flex flex-row gap-1">
             <Button variant="ghost" asChild>
                 <Link href="/"><ChevronLeft /></Link>
