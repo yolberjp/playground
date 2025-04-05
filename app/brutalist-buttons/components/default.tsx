a72ac085fafbb0e20b8ff771ded4533645095c21
Yolber JP <yolberjp@gmail.com>
2025-04-05 12:12:25 +0200

    style: Enhance button components with improved active state styles and group interaction effects

--- app/brutalist-buttons/components/default.tsx
+++ app/brutalist-buttons/components/default.tsx
@@ -1,7 +1,7 @@
 export default function DefaultButton({ children, ...props }: { children: React.ReactNode } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
         <button className="bg-amber-700 border-[1.5px] relative top-[2px] rounded-[8px] w-auto text-primary inline-block border-amber-700 text-center group disabled:opacity-50 disabled:cursor-not-allowed" {...props}>
-            <span className="relative text-center w-auto bg-amber-500 text-amber-900 border-amber-800 rounded-[8px] text-[15px] font-bold border-[1.5px] px-5 py-2 -translate-y-1 hover:-translate-y-1.5 active:-translate-y-0.5 mx-[-1.5px] group-disabled:hover:!-translate-y-1 block active:transition-all active:duration-100 select-none ">
+            <span className="relative text-center w-auto bg-amber-500 text-amber-900 border-amber-800 rounded-[8px] text-[15px] font-bold border-[1.5px] px-5 py-2 -translate-y-1 hover:-translate-y-1.5 active:-translate-y-0.5 mx-[-1.5px] group-disabled:hover:!-translate-y-1 group-disabled:active:!-translate-y-1 block active:transition-all active:duration-100 select-none">
             {children}
             </span>
         </button>
--- app/brutalist-buttons/components/ghost.tsx
+++ app/brutalist-buttons/components/ghost.tsx
@@ -1,7 +1,7 @@
 export default function GhostButton({ children, ...props }: { children: React.ReactNode } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
-        <button className="hover:bg-amber-500 border-[1.5px] relative top-[2px] rounded-[8px] w-auto text-primary inline-block border-transparent hover:border-amber-600 text-center group disabled:opacity-50 disabled:cursor-not-allowed" {...props}>
-            <span className="relative text-center w-auto group-hover:bg-amber-50 text-amber-900 border-transparent group-hover:border-amber-600 rounded-[8px] text-[15px] font-bold border-[1.5px] px-5 py-1 -translate-y-0.5 group-hover:-translate-y-0.75 active:-translate-y-0.5 mx-[-1.5px] group-disabled:hover:!-translate-y-1 block active:transition-all active:duration-100 select-none">
+        <button className="hover:bg-amber-500 active:bg-amber-500 border-[1.5px] relative top-[2px] rounded-[8px] w-auto text-primary inline-block border-transparent hover:border-amber-600 active:border-amber-600 text-center group disabled:opacity-50 disabled:cursor-not-allowed" {...props}>
+            <span className="relative text-center w-auto group-hover:bg-amber-50 group-active:bg-amber-50 text-amber-900 border-transparent group-hover:border-amber-600 group-active:border-amber-600 rounded-[8px] text-[15px] font-bold border-[1.5px] px-5 py-1 -translate-y-0.5 group-hover:-translate-y-0.75 group-active:-translate-y-0.75 active:-translate-y-0.5 mx-[-1.5px] group-disabled:hover:!-translate-y-1 block active:transition-all active:duration-100 select-none">
             {children}
             </span>
         </button>
