6214cb093b9f97d42d1c033d566c1453630b46aa
Yolber JP <yolberjp@gmail.com>
2025-04-09 19:58:19 +0200

    chore: Fix sintax error in BookingCalendar component

--- app/booking-calendar/page.tsx
+++ app/booking-calendar/page.tsx
@@ -1,11 +1,11 @@
+import InspiredBy from "@/components/inspiredBy";
 import Section from "@/components/section/section";
 import Reservation from "./components/reservation";
-import InspiredBy from "@/components/inspiredBy";
 
 
 export default function BookingCalendar() {
     return (
-        <Section title="Booking calendar" subtitle="a modern booking calendar" credits={<InspiredBy name="the fork" href="https://www.thefork.es/" />}>>
+        <Section title="Booking calendar" subtitle="a modern booking calendar" credits={<InspiredBy name="the fork" href="https://www.thefork.es/" />}>
             <Reservation />
         </Section>
       )
